0ae5cd8c38edeac35ca3a023d9cc692caf603af2
Ryan <vellabonelloryan@gmail.com>
2024-02-11 09:35:16 +0700

    lesson: Click Events

--- src/App.js
+++ src/App.js
@@ -5,11 +5,28 @@ import Footer from './components/Footer'
 function App() {
   const person = { name:'Ryan', age: 32 }
 
+  const foo = () => {
+    alert("Foo!!!!")
+  }
+
+  const goo = (name) => {
+    alert("Hello " + name + "!")
+  }
+
+  const hoo = (e) => {
+    alert(e.target)
+  }
+
   return (
     <div className="App">
       <Header />
       <main>
         { person.name } is { person.age } years old.
+        <div>
+          <button onClick={foo}>Click</button>
+          <button onClick={() => goo('Tony')}>Click</button>
+          <button onClick={(e) => hoo(e)}>Click</button>
+        </div>
       </main>
       <Footer />
     </div>
